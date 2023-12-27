19ae75977f2b17ed2ca2d7b90fd87209595c65d9
lesitoan <lesitoan.media@gmail.com>
2023-12-27 21:04:35 +0700

    addCode

--- 11-linkedList.cpp
+++ 11-linkedList.cpp
@@ -7,12 +7,12 @@ struct Node {
 	Node* next;
 }* first = NULL;
 
-void createLinkedList(int* arr, int n) {
-	first= new Node;
-	first->data = arr[0];
-	first->next = NULL;
+void createLinkedList(Node* &p, int* arr, int n) {
+	p= new Node;
+	p->data = arr[0];
+	p->next = NULL;
 	Node* t, * last;
-	last = first;
+	last = p;
 	for(int i = 1; i < n; i++) {
 		t = new Node;
 		t->data = arr[i];
@@ -67,16 +67,70 @@ Node* searchRecursion(Node* p, int x) {
 	return searchRecursion(p->next, x);
 }
 
+void insertNode(Node* &p, int pos, int x) {
+		Node* t = new Node;
+		t->data = x;
+	if(pos <= 0) { // before first Node
+		t->next = first;
+		p = t;
+	} else { //after pos
+		Node* q = p;
+		for(int i = 0; i < pos - 1 && q; i++) {
+			q = q->next;
+		}
+		if(!q) return; // pos > length
+		t->next = q->next;
+		q->next = t;
+	}
+}
+
+void insertLinkedListSorted(Node* &p, int x) {
+	Node* newNode = new Node;
+	newNode->data = x;
+	if(first->data > x) { // before first Node
+		newNode->next = p;
+		p = newNode;
+	} else {
+		Node* after = p, * before = NULL;
+		while(after && after->data < x) {
+			before = after;
+			after = after->next;
+		}
+		newNode->next = after;
+		before->next = newNode;
+	}
+}
+
+void deleteElement(Node* &p, int pos) {
+	if(pos == 1) {
+		p = p->next;
+	} else {
+		Node* t, *q = NULL;
+		t = p;
+		for(int i = 0; i < pos - 1 && t; i++) {
+			q = t;
+			t = t->next;
+		}
+		q->next = t->next;
+		delete t;
+	}
+} 
+
+
 int main() {
-	int arr[] = {9,8,7,6,5,4,3,2,1};
-	int n = 9;
-	createLinkedList(arr, n);
+	int arr[] = {3,5,7,9,11,15};
+	int n = 6;
+	createLinkedList(first, arr, n);
+//	displayLinkedlist(first);
+//	cout << "\n" << count(first);
+//	cout << "\n" << total(first) << endl;
+//	cout << maxOfLinkedList(first) << endl;
+//	cout << search(first, 7) << endl;
+//	cout << searchRecursion(first, 7) << endl;
+//	insertNode(first, 0, 99);
+//	insertLinkedListSorted(first,1);
+	deleteElement(first,1);
 	displayLinkedlist(first);
-	cout << "\n" << count(first);
-	cout << "\n" << total(first) << endl;
-	cout << maxOfLinkedList(first) << endl;
-	cout << search(first, 7) << endl;
-	cout << searchRecursion(first, 7) << endl;
 	getchar();
 	return 0;
 }
