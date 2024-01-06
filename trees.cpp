0e597713f931fdeadab2df3859226d14eda9a999
lesitoan <lesitoan.media@gmail.com>
2024-01-06 22:17:30 +0700

    add

--- trees.cpp
+++ trees.cpp
@@ -1,121 +1,125 @@
 #include <iostream>
+#include <stack>
+#include <queue>
 using namespace std;
 
-struct NodeOfTree {
-	NodeOfTree* lChild;
+struct Node {
+	Node* lChild;
 	int data; 
-	NodeOfTree* rChild;
+	Node* rChild;
 };
 
-struct NodeOfQueue {
-	NodeOfTree* data;
-	NodeOfQueue* next;
-};
-
-class Queue {
-private:
-	NodeOfQueue* front;
-	NodeOfQueue* rear;
-public:
-	Queue() {
-		front = rear = nullptr;
-	}
-	void enQueue(NodeOfTree* p) {
-		NodeOfQueue* t = new NodeOfQueue;
-		t->data = p;
-		t->next = nullptr;
-		if(!front) {
-			front = rear = t;
-		} else {
-			rear->next = t;
-			rear = t;
-		}
-	}
-	void deQueue() {
-		if(!front) {
-			cout << "enpty" << endl;
-		} else {
-			NodeOfQueue* t = front;
-			front = front->next;
-			delete t;
-		}
-	}
-	NodeOfTree* pop() {
-		if(!front) {
-			cout << "enpty" << endl;
-		} else {
-			return front->data;
-		}
-	}
-	
-	bool isEmpty() {
-		if(!front) return true;
-		return false;
-	}
-	~Queue(){
-		while(front) {
-			NodeOfQueue* t = front;
-			front = front->next;
-			delete t;
-		}
-	}
-};
 
 class Tree {
 private:
-	NodeOfTree* root;
-	Queue* queue = new Queue;
+	Node* root;
+	queue<Node*> queue;
 public:
-	Tree() {
-		root = new NodeOfTree;
-		root->lChild = nullptr;
-		root->rChild = nullptr;
-		queue->enQueue(root);
-	}
-	void create() {
-		int x = -1;
-		int y = -1;
-		NodeOfTree* p;
-		while(!queue->isEmpty()) {
-			cout << "Ezzzzzzzzzz " << endl;
-			p = queue->pop();
-			cout << "Enter data of left child: ";
-			cin >> x;
-			if(x != -1) {
-				cout << "x" << endl;
-				NodeOfTree* t = new NodeOfTree;
-				t->lChild = nullptr;
-				t->rChild = nullptr;
-				t->data = x;
-				p->lChild = t;
-				queue->enQueue(t);
-				x = -1;
-			}
-			cout << "Enter data of right child: ";
-			cin >> y;
-			if(y != -1) {
-				cout << "y" << endl;
-				NodeOfTree* t = new NodeOfTree;
-				t->lChild = nullptr;
-				t->rChild = nullptr;
-				t->data = y;
-				p->rChild = t;
-				queue->enQueue(t);
-				y = -1;
-			}
-			queue->deQueue();
-		}
-
-	}
+	Tree();
+	~Tree();
+	void create();
+	void preOrder(Node* t);
+    void preOrder() { preOrder(root); }
+    void iterativePreOrder();
+    void iterativePostOrder();
 };
+
 
 
 
 int main() {
 	Tree* tree = new Tree;
 	tree->create();
-	
+//	tree->iterativePreOrder();
+	tree->iterativePostOrder();
 	delete tree;
+	int k; cin >> k;
 	getchar();
 	return 0;
 }
+
+Tree::Tree() {
+		root = new Node;
+		root->lChild = nullptr;
+		root->rChild = nullptr;
+		queue.push(root);
+}
+
+Tree::~Tree() {
+	while(!queue.empty()) {
+		queue.pop();
+	}
+}
+
+void Tree::create() {
+	cout << "Enter Root Data:" << flush;
+	cin >> root->data;
+	int x;
+	Node* p;
+	while(!queue.empty()) {
+		p = queue.front();
+		cout << "Enter left child data of " << p->data << ": " << flush;
+		cin >> x;
+		if(x != -1) {
+			Node* t = new Node;
+			t->lChild = nullptr;
+			t->rChild = nullptr;
+			t->data = x;
+			p->lChild = t;
+			queue.push(t);
+		}
+		cout << "Enter right child data of " << p->data << ": " << flush;
+		cin >> x;
+		if(x != -1) {
+			Node* t = new Node;
+			t->lChild = nullptr;
+			t->rChild = nullptr;
+			t->data = x;
+			p->rChild = t;
+			queue.push(t);
+		}
+		queue.pop();
+	}
+}
+
+void Tree::preOrder(Node* t) {
+	if(t) {
+		cout << t->data << "\t";
+		preOrder(t->lChild);
+		preOrder(t->rChild);
+	}
+}
+
+void Tree::iterativePreOrder() {
+    	stack<Node*> st;
+    	Node* t = root;
+    	while(t || !st.empty()) {
+    		if(t) {
+    			cout << t->data << "\t";
+    			st.push(t);
+    			t = t->lChild;
+			} else {
+				t = st.top()->rChild;
+				st.pop();
+			}
+		}
+    	
+	}
+	
+void Tree::iterativePostOrder() {
+    	stack<Node*> st1;
+    	stack<int> st2;
+    	Node* t = root;
+    	st1.push(t);
+    	while(!st1.empty()) {
+    		t = st1.top();
+    		st1.pop();
+    		st2.push(t->data);
+    		if(t->lChild) st1.push(t->lChild);
+    		if(t->rChild) st1.push(t->rChild);
+		};
+		while(!st2.empty()) {
+			cout << st2.top() << "\t";
+			st2.pop();
+		}
+	}
